728b1cd9dc1f7650ac9eebdba112e5090d3d6538
Ye Min Aung <yeminaung.dev@gmail.com>
2026-08-30 11:15:54 +0630

    fix: build error on render

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,6 +1,3 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
-const envPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../../.env");
-dotenv.config({ path: envPath });
+dotenv.config();
